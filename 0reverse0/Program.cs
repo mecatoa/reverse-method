5905ff111cabd827f3caaf15371047a074c2a7e1
ItShop <ItShop@DESKTOP-CTGQ2OD>
2021-11-27 20:20:56 +0200

    Add project files.

---
+++ 0reverse0/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0reverse0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LinkedList<string> MyList = new LinkedList<string>();
+
+            MyList.AddFirst("OK");
+            MyList.AddLast("K.O");
+            LinkedListNode<String> MyNode = new LinkedListNode<string>("Done");
+            MyList.AddFirst(MyNode);
+            MyList.AddBefore(MyNode, "Tool");
+            MyList.AddAfter(MyNode, "Cool");
+
+            Console.WriteLine("The LinkedList Before Reversing: ");
+
+            foreach (var item in MyList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("The LinkedList After Reversing: ");
+
+            foreach (var item in MyList.Reverse())
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
